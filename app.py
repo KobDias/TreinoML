81d35c081ebf50fe0c1cc8764ebd364e56c80531
KobDias <sofhia.dias@etec.sp.gov.br>
2025-03-30 20:23:31 -0300

    funcional

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import joblib
+
+modelo= joblib.load('knn.pkl') # carregando o modelo pre treinado em pkl
 
 app = Flask(__name__)
 
@@ -8,12 +11,19 @@ def home():
 
 @app.route('/prediz', methods=['POST'])
 def prediz():
-    request.form.get(float('sepal_length'))
-    request.form.get(float('sepal_width'))
-    request.form.get(float('petal_length'))
-    request.form.get(float('petal_width'))
+    try:
+        sepal_length = float(request.form.get('sepal_length'))
+        sepal_width = float(request.form.get('sepal_width'))
+        petal_length = float(request.form.get('petal_length'))
+        petal_width = float(request.form.get('petal_width'))
 
+        predicao = modelo.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+        especies = predicao[0] # o resultado da predição
 
+        return render_template('index.html', especies=especies)
+    
+    except Exception as e:
+        return render_template('index.html', error=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
